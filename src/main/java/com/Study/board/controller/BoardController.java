6ee8c204aef50317c9399b1fc0da647949b238ae
ckdkal <126321248+ckdkal@users.noreply.github.com>
2023-10-16 18:40:13 +0900

    스프링부트 게시판 만들기

---
+++ src/main/java/com/Study/board/controller/BoardController.java
@@ -0,0 +1,96 @@
+package com.Study.board.controller;
+
+import com.Study.board.entity.Board;
+import com.Study.board.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+
+@Controller
+public class BoardController {
+
+    @Autowired
+    private BoardService boardService;
+    @GetMapping("/board/write") //localhost:8090/board/write
+    public String boardWriteForm() {
+
+        return "boardwrite";
+    }
+    @PostMapping("/board/writepro")
+    public String boardWritePro(Board board, Model model, MultipartFile file) throws Exception {
+
+        boardService.write(board, file);
+
+        model.addAttribute("message", "글 작성이 완료되었습니다.");
+        model.addAttribute("searchUrl", "/board/list");
+
+        return "message";
+    }
+
+    @GetMapping("/board/list")
+    public String boardList(Model model,
+                            @PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.DESC)
+                            Pageable pageable, String searchKeyword) {
+
+        Page<Board> list = null;
+
+        if(searchKeyword == null) {
+            list = boardService.boardList(pageable);
+        } else {
+            list = boardService.boardSearchList(searchKeyword, pageable);
+        }
+
+        int nowpage = list.getPageable(). getPageNumber() + 1;
+        int startPage = Math.max(nowpage - 4, 1);
+        int endpage = Math.min(nowpage + 5, list.getTotalPages());
+
+        model.addAttribute("list", list);
+        model.addAttribute("nowPage", nowpage);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endpage);
+
+        return "boardlist";
+    }
+
+    @GetMapping("/board/view") // localhost:8090/board/view?id=1 (get 방식 파라미터)
+    public String boardView(Model model, Integer id) {
+        model.addAttribute("board", boardService.boardview(id));
+        return "boardview";
+    }
+
+    @GetMapping("/board/delete")
+    public String boardDelete(Integer id) {
+
+        boardService.boardDelete(id);
+        return "redirect:/board/list";
+    }
+
+    @GetMapping("/board/modify/{id}")
+    public String boardModify(@PathVariable("id") Integer id, Model model) {
+
+        model.addAttribute("board",boardService.boardview(id));
+
+        return "boardmodify";
+    }
+
+    @PostMapping("board/update/{id}")
+    public String boardUpdate(@PathVariable("id") Integer id, Board board, MultipartFile file) throws Exception {
+
+        Board boardTemp = boardService.boardview(id);
+        boardTemp.setTitle(board.getTitle());
+        boardTemp.setContent(board.getContent());
+
+        boardService.write(boardTemp, file);
+
+        return "redirect:/board/list";
+    }
+}
---
+++ src/test/java/com/Study/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Study.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
